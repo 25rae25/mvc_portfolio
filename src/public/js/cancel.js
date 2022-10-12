fb013a73b852b5d88373ab4d946c70fcefabc7db
25rae25 <whdudfo0124@gmail.com>
2022-10-12 18:04:14 +0900

    Create 수정 취소

--- src/public/js/cancel.js
+++ src/public/js/cancel.js
@@ -3,7 +3,7 @@ function cancelWrite() {
   window.location.href = '/login';
 }
 
-// function cancelWrite() {
-//   alert('회원가입을 취소했습니다');
-//   window.location.href = '/login';
-// }
+function cancelUpdate() {
+  alert('수정을 취소했습니다');
+  window.location.href = '/health?id=1';
+}
